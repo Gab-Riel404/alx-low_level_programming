af1aca374d989be754be7dc372f836f395d53782
Gabriel Asiedu Ntim <deleir404@gmail.com>
2023-04-18 22:51:42 -0700

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-
-/*
- * File: dog.h
- * Desc: Header file that defines a new type struct dog.
- */
-
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
 /**
- * struct dog - A new type describing a dog.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
+ * struct dog - struct for dog
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: dog owner
  */
+
 struct dog
 {
 	char *name;
@@ -29,4 +27,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif
+#endif /* DOG_H */
