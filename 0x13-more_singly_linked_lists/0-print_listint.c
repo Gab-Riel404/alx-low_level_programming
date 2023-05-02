8b6882fa44af45e44679cc1281ea66c12ff5f550
Gabriel Asiedu Ntim <deleir404@gmail.com>
2023-05-02 10:18:48 -0700

    commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,17 +3,21 @@
 
 /**
  * print_listint - Prints all the elements of a listint_t list.
- * @h: A pointer to the head of the list_t list.
+ * @d: A pointer to the head of the list_t list.
+ *
  * Return: The number of nodes in the list_t list.
  */
-size_t print_listint(const listint_t *h)
+size_t print_listint(const listint_t *d)
 {
 	size_t nodes = 0;
-	while (h)
+
+	while (d)
 	{
 		nodes++;
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", d->n);
+		d = d->next;
 	}
+
 	return (nodes);
 }
+
